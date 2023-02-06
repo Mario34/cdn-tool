158467510bea40233e397fe5e8d1c7021d916385
Mario34 <zhl.mario34@outlook.com>
2023-02-06 18:25:23 +0800

    chore: init

---
+++ modules.js
@@ -0,0 +1,19 @@
+/**
+ * @typedef {Object} Modules
+ * @property {string} name
+ * @property {string} version
+ */
+
+/**
+ * @type {Modules[]}
+ */
+export const cdnModules = [
+  {
+    name: 'vue',
+    version: '3.2.45',
+  },
+  {
+    name: 'vue-router',
+    version: '4.1.6',
+  },
+]
